d89bec37cb43694caff54d6668c51b06be15e473
davidgarciasolis <davidgarciasolis@gmail.com>
2026-01-25 10:24:24 +0000

    Correccion de ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -16,12 +16,22 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*s;
-	int		size;
+	char	c;
 
-	s = ft_itoa(n);
-	size = ft_strlen(s);
-	write(fd, s, size);
+	if (n == -2147483648)
+		write(fd, "-2147483648", 11);
+	else
+	{	
+	if (n < 0)
+	{
+		n = -n;
+		write(fd, "-", 1);
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
+	}	
 }
 /*
 int	main(void)
