b34492b7ef59ae55e83578d0190b90d3dd78e05e
David <davidgarciasolis@gmail.com>
2026-01-26 23:29:02 +0000

    Fin de la correccion

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,33 +12,39 @@
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*check_counter(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*tmp;
+	t_list	*new_node;
 	void	*content;
 
-	if (!lst || !f || !del)
-		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
 		content = f(lst->content);
-		if (content)
+		if (content == NULL)
 		{
-			tmp = ft_lstnew(content);
-			if (!tmp)
-			{
-				ft_lstclear(&new_lst, del);
-				return (NULL);
-			}
-			ft_lstadd_back(&new_lst, tmp);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
 		}
-		else
-			ft_lstdelone(new_lst, del);
+		new_node = ft_lstnew(content);
+		if (new_node == NULL)
+		{
+			del(content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
+}
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	if (!lst || !f || !del)
+		return (NULL);
+	return (check_counter(lst, f, del));
 }
 /*
 int	main(void)
